90ef3b8c7ff5fcf8ede57e7a424b448732467928
Keiji Uehata <keiji.ue@gmail.com>
2018-05-25 23:27:36 +0900

    fixed refact

--- client_start.sh
+++ client_start.sh
@@ -1,6 +1,3 @@
 docker build -t ansible .
-docker run --name ansible --rm -v `pwd`:/tmp/ -itd ansible sh
-docker exec ansible mkdir /root/.ssh/
-docker cp ~/.ssh/1mg_rsa ansible:/root/.ssh/id_rsa
-docker exec ansible chmod 400 /root/.ssh/id_rsa
+docker run --name ansible --rm -v /root/.ssh:/root/.ssh -v `pwd`:/tmp/ -itd ansible sh
 docker exec -it ansible sh
