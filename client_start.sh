7cfbd8e5702770e6aad3f787dfbdc5c71d06472b
keiji.uehata <keiji.uehta@rakus.co.jp>
2018-06-06 13:00:11 +0900

    add Dockerfile

--- client_start.sh
+++ client_start.sh
@@ -1,4 +1,4 @@
 docker build -t ansible .
 mkdir /tmp/ansible_logs/`date +%Y%m%d`
-docker run --name ansible --rm -v /tmp/ansible_logs/`date +%Y%m%d`:/tmp/ansible_logs -v /root/.ssh:/root/.ssh -v `pwd`:/ansible -itd ansible sh
+docker run --name ansible --rm -v /root/.ssh:/root/.ssh -v /tmp/ansible_logs/`date +%Y%m%d`:/tmp/ansible_logs -v /root/.ssh:/root/.ssh -v `pwd`:/ansible -itd ansible sh
 docker exec -it ansible sh
