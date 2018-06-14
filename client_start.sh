2de8de7a04e0c0b2256e31589b0b12c89b3f68bb
keiji.uehata <keiji.uehta@rakus.co.jp>
2018-06-14 09:02:31 +0900

    fixed client_start.sh

--- client_start.sh
+++ client_start.sh
@@ -1,3 +1,4 @@
+ln -s Dockerfile.alpine Dockerfile
 docker build -t ansible .
 mkdir /tmp/ansible_logs/`date +%Y%m%d`
 docker run --privileged --name ansible --rm -v /root/.ssh:/root/.ssh -v /tmp/ansible_logs/`date +%Y%m%d`:/tmp/ansible_logs -v /root/.ssh:/root/.ssh -v `pwd`:/ansible -itd ansible sh
