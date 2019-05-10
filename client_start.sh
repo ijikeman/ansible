0b2920020fbb89f9843f614dfc41ac4d9040bb3f
keiji.uehata <keiji.uehta@rakus.co.jp>
2019-05-10 14:35:29 +0900

    Dockerfile.archlinux

--- client_start.sh
+++ client_start.sh
@@ -1,4 +1,4 @@
-ln -s Dockerfile.centos7 Dockerfile
+ln -s Dockerfile.archlinux Dockerfile
 docker build -t ansible .
 mkdir /tmp/ansible_logs/`date +%Y%m%d`
 docker run --privileged --name ansible --rm -v /root/.ssh:/root/.ssh -v /tmp/ansible_logs/`date +%Y%m%d`:/tmp/ansible_logs -v /root/.ssh:/root/.ssh -v `pwd`:/ansible -itd ansible sh
